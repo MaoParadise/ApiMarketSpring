798310dce35b7e26278c3590c2fed88c1ce83695
Carlos Pasten Bravo <carlos92.pastenbravo@gmail.com>
2022-02-12 15:28:20 -0300

    clase 17#

--- src/main/java/com/spring/market/persistence/entity/Categoria.java
+++ src/main/java/com/spring/market/persistence/entity/Categoria.java
@@ -1,10 +1,13 @@
 package com.spring.market.persistence.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +22,9 @@ public class Categoria {
     private String descripcion;
 
     private Boolean estado;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
 
     public Integer getIdCategoria() {
         return idCategoria;

f444dcf3e81493fa21eaf311ae2439b8c7d5b3b5
Carlos Pasten Bravo <carlos92.pastenbravo@gmail.com>
2022-02-13 16:04:01 -0300

    clase 20 y 21

---
+++ src/main/java/com/spring/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,32 @@
+package com.spring.market.persistence.mapper;
+
+import java.util.List;
+
+import com.spring.market.domain.Product;
+import com.spring.market.persistence.entity.Producto;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = { CategoryMapper.class })
+public interface ProductMapper {
+    
+    @Mappings({
+        @Mapping(source = "idProducto", target = "productId"),
+        @Mapping(source = "nombre", target = "name"),
+        @Mapping(source = "idCategoria" , target = "categoryId"),
+        @Mapping(source = "precioVenta" , target = "price"),
+        @Mapping(source = "cantidadStock", target = "stock"),
+        @Mapping(source = "estado", target = "active"),
+        @Mapping(source = "categoria", target = "category"),
+    })
+    Product toProduct(Producto producto);
+    List<Product> toProduct(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+    List<Producto> toProducto(List<Product> products);
+}

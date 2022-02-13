89e105ecf0420df7d6f8100ef4d605833494a97d
Carlos Pasten Bravo <carlos92.pastenbravo@gmail.com>
2022-02-13 16:29:17 -0300

    clase 22 y 23

---
+++ src/main/java/com/spring/market/persistence/ProductoRepository.java
@@ -0,0 +1,56 @@
+package com.spring.market.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.spring.market.domain.Product;
+import com.spring.market.domain.repository.ProductRepository;
+import com.spring.market.persistence.crud.ProductoCrudRepository;
+import com.spring.market.persistence.entity.Producto;
+import com.spring.market.persistence.mapper.ProductMapper;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductoRepository implements ProductRepository{
+    
+    private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+
+    @Override
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProduct(productos);
+    }
+
+
+    @Override
+    public void delete(Integer productId) {
+        productoCrudRepository.deleteById(productId);
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(Integer categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProduct(productos));
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(Integer quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProduct(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(Integer productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+    }
+
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+
+}
